7047dc917b8d74b1b9c08ae553fcf55085d771f2
Matvii Artemenko <100190098+MatveyArtemenko@users.noreply.github.com>
2023-04-17 10:12:17 +0100

    Initial Commit

---
+++ Wakelet_demo/Wakelet_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wakelet_demoApp.swift
+//  Wakelet_demo
+//
+//  Created by admin on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Wakelet_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
